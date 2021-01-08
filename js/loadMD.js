bd6e98c4389cf4d8eee3501d2b4c2b7e7d00cd78
ChiaChing-Yen <ccyen@umich.edu>
2021-01-08 16:34:47 +0800

    alpha

--- js/loadMD.js
+++ js/loadMD.js
@@ -7,3 +7,21 @@ function loadRemoteMDtoHtml(_url) {
         html = converter.makeHtml(text);
     document.write(html);
 }
+
+function loadLocalMDtoHtml(path) {
+    var converter = new showdown.Converter(),
+        text = loadFile(path),
+        html = converter.makeHtml(text);
+    document.write(html);
+}
+
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status == 200) {
+        result = xmlhttp.responseText;
+    }
+    return result;
+}
---
+++ md/resources/download.md
@@ -0,0 +1,16 @@
+## Download
+
+### Documents
+* Download link
+* Download link
+***
+### Training Manuals
+* Download link
+* Download link
+***
+### Robot models
+* Download link
+* Download link
+### In-House Grasshopper Plugin
+* Download link
+* Download link
